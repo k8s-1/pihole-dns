70a96ad79f3c508cdac3994afa6b1ccfef82bb89
pman <pman-$-432894328923@gmail.com>
2024-09-05 09:50:53 +0200

    ...

--- run.sh
+++ run.sh
@@ -7,4 +7,7 @@
 # set the admin password
 docker-compose run pihole pihole -a -p
 
-echo "instance is up and running @ http://localhost:8080/admin/login.php"
+echo "instance is up and reachable @ http://localhost:8080/admin/login.php"
+
+DNS_SERVER_IP=$(docker inspect pihole | jq -r '.[0].NetworkSettings.Networks.pihole_default.IPAddress')
+dig @"$DNS_SERVER_IP"
